2e5c92533362654471bee4128f962e2ad3a6312e
Navyatha-07 <chinthanavyatha.1.4@gmail.com>
2025-11-29 21:27:13 +0530

    Done with all the forms in ngo

--- 7_NGOSignup.php
+++ 7_NGOSignup.php
@@ -3,12 +3,15 @@ $servername = "localhost";
 $username = "root"; 
 $password = "";
 $dbname = "sahyog1";
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullname = $_POST['fullname'] ;
     $email = $_POST['email'];
@@ -16,27 +19,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mobile_number=$_POST['mobile_number'];
      $nameofNGO=$_POST['nameofNGO'];
       $location=$_POST['location'];
-    // Hash the password
+
     $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
     $check = $conn -> prepare("SELECT id from ngo_users WHERE mobile_number =? OR email=?");
     $check-> bind_param("ss",$mobile_number,$email);
     $check-> execute();
     $check->store_result();
+
+    if(!isset($_POST['email'])){
+        die("Email field not sent-your form name or pattern is wrong");
+    }
     if($check -> num_rows>0){
         echo "Mobile number or email already exists.Please try again.";
     }
+
     else{
     // Insert into DB
     $sql = "INSERT INTO ngo_users (fullname, email, password,mobile_number,nameofNGO,location) VALUES (?, ?, ?,?,?,?)";
     $stmt = $conn->prepare($sql);
+
     if($stmt == false){
         die("prepare failed".$conn->error);
     }
+
     $stmt->bind_param("ssssss", $fullname, $email, $hashedPassword,$mobile_number,$nameofNGO,$location);
     if ($stmt ->execute()) {
         echo "Signup successful! <br><br>";
         echo '<a href="Login.html"><button type="button">Go to Login</button></a>';
-    } else {
+    }
+     else {
         echo "Error: " . $stmt->error;
     }
     $stmt->close();
